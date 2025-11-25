585daf7fcf4d75e1de678f67714af8e46c39445f
Ashkar S <ashkars178@gmail.com>
2025-11-25 10:41:08 +0530

    do

--- index.js
+++ index.js
@@ -3,4 +3,16 @@ const http=require('http')
 
 
 //create server
-const server=http.createServer()
+const server=http.createServer((req,res)=>{
+    if(req.url=='/getUsers'){
+        res.end('hello User')
+    }else if(req.url=='/getBooks'){
+        res.end('hello Noooks')
+    }
+})
+
+const port=3000
+
+server.listen(port,()=>{
+    console.log(port)
+})
